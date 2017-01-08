3ff7923a052dad574b219840073688b914c1f8b8
Bartosz Sledzinski <sledzinski.bartosz@gmail.com>
2017-01-08 20:38:08 +0100

    JFDZ3GZ-25 Dodano jawny formularz przeciw botom

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ $('.navbar-nav a').click(function () {
     $("html, body").animate({ scrollTop:$($(this).attr('href')).offset().top }, "slow");
 })
 });
+
+
+/* Invitation */
+
+function antibot2 (event) {
+
+    return document.forms.mailsend.antibot.value=='5';
+}
+
+//////
