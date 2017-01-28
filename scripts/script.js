0b0ab680f5e84f9c130ceda56ddddd932f713190
sledziu87 <sledzinski.bartosz@gmail.com>
2017-01-28 02:06:46 +0100

    JFDZ3GZ-27 Nadano grubszą masę navbarowi

--- scripts/script.js
+++ scripts/script.js
@@ -40,15 +40,16 @@ $(function () {
 
 /* Invitation */
 
-// Zabezpieczenie wysyłki formularza antybotem
-function antibot2 (event) {
-    return document.forms.mailsend.antibot.value == RandomValue;
-}
-// losowość zabezpieczenia antybot
-var randomTab = [Math.floor(Math.random()*10)+1, Math.floor(Math.random()*10)+1];
-var RandomValue = randomTab[0] + randomTab[1];
+    // Zabezpieczenie wysyłki formularza antybotem
+    function antibot2 (event) {
+        return document.forms.mailsend.antibot.value == RandomValue;
+    }
+    // losowość zabezpieczenia antybot
+    var randomTab = [Math.floor(Math.random()*10)+1, Math.floor(Math.random()*10)+1];
+    var RandomValue = randomTab[0] + randomTab[1];
 
 $(document).ready(function () {
+
     $ ('input[name=antibot]').attr("placeholder", (randomTab[0] + "+" + randomTab[1] + "=" + "?"));
 
     // ukrywanie/pokazywanie antybota
@@ -56,16 +57,59 @@ $(document).ready(function () {
     $ ('input[name=email]').focus(function () {
         $('section.BotSection').show();
     });
-});
 
-/* Responsiwny pasek menu */
+    /* Responsiwny pasek menu */
 
-$(document).on('scroll',function () {
-   if ($(this).scrollTop()>738) {
-       console.log("dodaj");
-    }
-    else if ($(this).scrollTop()<737){
+    // /*uzależnienie wystąpienia od roździelczośći*/
+    // $(window).resize(function(){
+    //     if ($('header').width() <= 1200 ){
 
-        console.log("odejmij")
-    }
-        });
+            var zmiennaA = $('.nav>li>a').css('padding'),  //TODO: nadać prawidłowe nazwy zmiennym
+                zmiennaB = $('.navbar-brand img').css('height'),
+                zmiennaC = $('Body').css('font-size');
+
+            // function testujacaPolozenieScrolla() {
+            var test = {position : $(document).offset().top-150};
+
+
+            // }
+            $(window).load(function () {
+            zmiennaA = parseInt(zmiennaA,10);
+            zmiennaB = parseInt(zmiennaB,10);
+            zmiennaC = parseInt(zmiennaB,10);
+
+            $('.nav>li>a').css('padding',zmiennaA * 2);
+            $('.navbar-brand > img').css({'height': zmiennaB * 1.1 , 'margin' : zmiennaB * 0.01 });
+            $('Body').css('font-size',zmiennaC * 0.4);
+            });
+
+        // }   /// Nie działa :/
+    // }); //koniec f.resize
+
+}); // koniec f.ready
+
+// $(document).on('scroll',function () {
+// //    // function testujacaPolozenieScrolla() {
+// var test = { position : $(document).offset().top-150 };
+//     if ($(this).scrollTop()>test){
+//         console.log("dodaj"); }
+// });
+
+//
+// $(document).on('scroll',function () {
+//
+//    // if ($(this).scrollTop()>738) {
+//    //     console.log("dodaj");
+//    //  }
+//    //  else if ($(this).scrollTop()<737){
+//    //
+//    //      console.log("odejmij")
+//    //  }
+//
+//     // if ($ (this).scrollTop()
+//
+//     console.log("DecOfPosition.siteTitle.position");
+//     var klucz1='siteTitle';
+//     var klucz2='.position';
+//     console.log("DecOfPosition.siteTitle.position");
+// });
