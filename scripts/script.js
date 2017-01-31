66ea07b9f8fd63aa734a3aa0fd5fe7843923768f
sledziu87 <sledzinski.bartosz@gmail.com>
2017-01-31 22:58:43 +0100

    JFDZ3GZ-27 Zamiana dodawania klasy na odbieranie jej; Dopisano klasy dla IMG i Linków paska nawigacji; Odpowiedni zmodernizowano script. EFEKT FINALNY: Pogrubienie jest uzależnione od MediaQ oraz Odrazu widoczne (> 993px)

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,14 @@
 $(function () {
 
 $('.navbar-nav a').click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
     $("html, body").animate({ scrollTop:$($(this).attr('href')).offset().top }, "slow");
 })
 });
 
 // zmienny tytuł strony
-    var siteTitles = [];
-
 $(function () {
-
+    var siteTitles = [];
     var newTitle = ".swapp";
 
     $('.navbar-nav a').each(function () {
@@ -33,74 +31,26 @@ $(function () {
                 newTitle = siteTitle.title;
             }
         });
-
-        // TODO: TU WSTAWIE AKCJE SCROLLA + coś zrobić z tym
+        //modyfikacja gabarytu paska nawigacji
         if( scrollTop < siteTitles[0].position ){
-            $('.navbar-brand img').addClass('imgModyfication');
-            $('.nav>li>a').addClass('linkModyfication');
-
+            $('.navbar-brand img').addClass('imgMod');
+            $('.nav>li>a').addClass('linkMod');
+        }else {
+            $('.nav>li>a').removeClass('linkMod');
+            $('.navbar-brand img').removeClass('imgMod');
         }
-        else {
-            // changeMenuSize(2, 1.1, 0.01, 1.5);
-            $('.nav>li>a').removeClass('linkModyfication');
-            $('.navbar-brand img').removeClass('imgModyfication');
-        }
-
         $('title').text(newTitle);
-
     });
-});
-//
-//
-//     //operacje na rozmiarze paska nawigacji
-// var $zmiennaA, $zmiennaB, $zmiennaB2, $zmiennaC;
-//
-// function changeMenuSize(padding, height, margin, font){
-//     $('.nav>li>a').css('padding', $zmiennaA * padding);
-//     $('.navbar-brand > img').css({'height': $zmiennaB * height, 'margin': $zmiennaB2 * margin});
-//     $('Body').css('font-size', $zmiennaC * font);
-// }
-//
-//
-//
-//         $zmiennaA = $('.nav>li>a').css('padding'),
-//         $zmiennaB = $('.navbar-brand img').css('height'),
-//         $zmiennaB2 = $('.navbar-brand img').css('margin'),
-//         $zmiennaC = $('Body').css('font-size');
-//
-//     $zmiennaA = parseInt($zmiennaA);
-//     $zmiennaB = parseInt($zmiennaB);
-//     $zmiennaB2 = parseInt($zmiennaB2);
-//     $zmiennaC = parseInt($zmiennaC);
-//padding, height margin font
-//     changeMenuSize(2, 1.1, 0.01, 1.5);
-//
-//
-//
-//     // substytut MediaQuery dla wielkości paska nav
-//     $(window).resize(function(){
-//         var largeScreen = $(document).width();
-//         if( largeScreen < 1000 ){
-//             changeMenuSize(1,1,1,1)
-//         }
-//         else if ( largeScreen >=1000 ) {        // błąd przy operacjach na zsuwaniu okna - program nie wie ktory warunek ugryzc
-//             changeMenuSize(2, 1.1, 0.01, 1.5);
-//         }
-//     });
-
-$(document).ready(function () {
 
     //matematyka dla zadania z antybota
     $ ('input[name=antibot]').attr("placeholder", (randomTab[0] + "+" + randomTab[1] + "=" + "?"));
-
     // ukrywanie/pokazywanie antybota
     $ ('section.BotSection').hide();
     $ ('input[name=email]').focus(function () {
         $('section.BotSection').show();
     });
 
-
-}); // koniec f.ready
+});
 
 /* Invitation */
 
